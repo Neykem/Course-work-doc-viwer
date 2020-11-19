baad53c87afafdcb6229db2d1b070e268c537ad1
Neykem <33416493+Neykem@users.noreply.github.com>
2020-11-19 14:46:28 +0300

    Load_2

---
+++ Model/UrlDocs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Course_work_doc_lib.Model
+{
+    class UrlDocs
+    {
+        //[Key]
+        public int id { get; set; }
+        public string url { get; set; }
+
+        public UrlDocs()
+        {
+
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Course_work_doc_lib.Model
+{
+    public class User
+    {
+        [Key]
+        private int id;
+        private string login;
+        private string password;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public User()
+        {
+
+        }
+    }
+}
